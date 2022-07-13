1bb6ad8a3c82d5dd9e45c76aa72956da50145974
unknown <adetayoayodeji7@gmail.com>
2022-07-13 11:40:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import SingleColor from './SingleColor';
+import Values from 'values.js';
 
 function App() {
+  const [color, setColor] = useState('')
+  const [error, setError] = useState(false)
+  const [list, setList] = useState([])
+  // console.log(list)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10) ;
+      setList(colors)  
+    } catch (error) {
+      setError(true)      
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          value={color} 
+          onChange={(e) => setColor(e.target.value)}
+          className={`${error?'error' : null}`}/>
+          <button className="btn" placeholder='#222'>Submit</button>
+        </form>
+      </section>
+
+      <section className="colors">
+        {list.map((color,index) => {
+          return <SingleColor {...color} key={color}  index={index}/>
+        })}
+      </section>    
+    </>
+
   );
 }
 
---
+++ src/SingleColor.js
@@ -0,0 +1,35 @@
+import { clear } from '@testing-library/user-event/dist/clear';
+import React,{useState, useEffect} from 'react';
+import rgbToHex from './utils';
+
+const SingleColor = ({rgb, weight,index}) => {    
+    const [alert, setAlert] = useState(false);
+    const bcg = rgb.join(',')
+    const hex = rgbToHex(...rgb)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setAlert(false)
+        }, 3000)
+        return () => clearTimeout(timeout)
+    }, [alert])
+  return (
+    <article index = {index}
+        className= {`color ${index >10 && 'color-light'}`} 
+        style={
+            {backgroundColor : `rgb(${bcg})`} 
+        } 
+        onClick ={ () => {
+            setAlert(true)
+
+            navigator.clipboard.writeText(hex)
+    }}>
+        <p className="percent-value">{weight}%</p>
+        <p className="color-value">{hex}</p>
+        <p>{console.log(index)}</p>
+        {alert && <p className='alert'>Copy to Clipboard</p>}
+    </article>
+  )
+}
+
+export default SingleColor
